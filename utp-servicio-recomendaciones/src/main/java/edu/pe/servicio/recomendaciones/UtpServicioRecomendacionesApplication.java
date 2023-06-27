4c6dd34cc263d70392d13a399ff40e3549f1f8e1
Guz <DarckProyect8@gmail.com>
2023-06-27 18:04:18 -0500

    eurake server

--- utp-servicio-recomendaciones/src/main/java/edu/pe/servicio/recomendaciones/UtpServicioRecomendacionesApplication.java
+++ utp-servicio-recomendaciones/src/main/java/edu/pe/servicio/recomendaciones/UtpServicioRecomendacionesApplication.java
@@ -2,8 +2,10 @@ package edu.pe.servicio.recomendaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UtpServicioRecomendacionesApplication {
 
 	public static void main(String[] args) {
